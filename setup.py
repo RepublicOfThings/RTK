15758f24b582b7fc49dd5d5a76d61b80b3d7c06c
MarkD <mark.l.douthwaite@gmail.com>
2018-10-21 00:51:52 +0100

    2.0.2 - Added a basic authentication layer and user DB.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='rtk',
-      version="2.0.1c",
+      version="2.0.2",
       description='RTK',
       author_email='mark@douthwaite.io',
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
