9e7150058c61a2fb32d595eeb12d1dfcab6400f4
MarkD <mark.l.douthwaite@gmail.com>
2018-10-20 23:46:06 +0100

    2.0.1b - Added corrections to project templates. Added some logging capabilities.

--- rtk/core/commands.py
+++ rtk/core/commands.py
@@ -1,4 +1,5 @@
 import os
+from rtk.utils.logs import build_log
 from rtk import RTK_CACHE
 from rtk.core.application import RTKApp
 from rtk.core.processing import render, process_paths, process_app_name, process_splunk_credentials
@@ -6,31 +7,48 @@ from rtk.utils import template_path
 
 
 def generate(config, fmt="yml"):
+    build_log.info("Generating your project...", extra=dict(event="CLI", meta="| IN PROGRESS"))
     client_path = process_paths(config, RTK_CACHE)
 
     config = process_splunk_credentials(config)
     config = process_app_name(config)
-
+    build_log.info("Processed client details...", extra=dict(event="CLI", meta="| IN PROGRESS"))
     z = render(template_path(f"{config.template}.yml-tpl"), config.__dict__)
+    build_log.info("Rendered the project template...", extra=dict(event="CLI", meta="| IN PROGRESS"))
     path = os.path.join(client_path, config.name+f".{fmt}")
 
     with open(path, "w") as file:
         file.write(z)
+    build_log.info("Project was successfully generated.", extra=dict(event="CLI", meta="| COMPLETE"))
 
 
 def deactivate(config, **kwargs):
+    build_log.info(f"Deactivating '{config.client}/{config.name}'",
+                   extra=dict(event="CLI", meta="| IN PROGRESS"))
     app = RTKApp.from_cache(config, **kwargs)
     app.deactivate()
+    build_log.info(f"Deactivated '{config.client}/{config.name}'",
+                   extra=dict(event="CLI", meta="| COMPLETE"))
 
 
 def activate(config, **kwargs):
+    build_log.info(f"Activating '{config.client}/{config.name}'",
+                   extra=dict(event="CLI", meta="| IN PROGRESS"))
     app = RTKApp.from_cache(config, **kwargs)
     app.activate()
+    build_log.info(f"Activated '{config.client}/{config.name}'",
+                   extra=dict(event="CLI", meta="| COMPLETE"))
 
 
 def build(config, **kwargs):
+    build_log.info(f"Building your application '{config.name}'...",
+                   extra=dict(event="CLI", meta="| IN PROGRESS"))
     app = RTKApp.from_cache(config, **kwargs)
     app.build()
+    build_log.info(f"Your application was successfully built!",
+                   extra=dict(event="CLI", meta="| COMPLETE"))
+    build_log.info(f"You can now go ahead and activate your application.",
+                   extra=dict(event="CLI", meta="| TIP"))
 
 
 def delete(config, **kwargs):
